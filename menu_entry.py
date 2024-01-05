b93aed499d75e9e2ea19506c6253a8ed0a24236d
Manuel Wiese <m.wiese@kaitos.ai>
2024-01-05 10:58:55 +0100

    Refactored menu code to menu.py and implemented menus for different tasks

--- menu_entry.py
+++ menu_entry.py
@@ -7,6 +7,13 @@ class MenuEntry(ABC):
     def __str__(self) -> str:
         ...
 
+    @abstractmethod
+    def get_return_value(self) -> str:
+        ...
+
+    def to_string(self, **kwargs)-> str:
+        return str(self)
+
 
 class Command(MenuEntry):
     def __init__(self, command, user_name, time, host) -> None:
@@ -20,7 +27,22 @@ class Command(MenuEntry):
         return cls(**dictionary)
 
     def __str__(self) -> str:
-        return f"{self.host} {self.command}"
+        return f"{self.command}"
+    
+    def to_string(self, show_user_name=True, show_host=True, show_time=True):
+        repr_length = 10
+        user_name_repr = f"{self.user_name[:repr_length]:<{repr_length}}" if show_user_name else ""
+        host_repr = f"{self.host[:repr_length]:<{repr_length}}" if show_host else ""
+        time_repr = f"{self.time[:repr_length]:<{repr_length}}" if show_time else ""
+        command_repr = self.command
+
+        values = [user_name_repr, host_repr, time_repr, command_repr]
+        values = [value for value in values if value]
+
+        return " | ".join(values)
+
+    def get_return_value(self) -> str:
+        return self.command
 
 
 class Host(MenuEntry):
@@ -28,6 +50,9 @@ class Host(MenuEntry):
         self.host = host
     
     def __str__(self) -> str:
+        return self.host
+    
+    def get_return_value(self) -> str:
         return self.host
 
 
@@ -37,3 +62,14 @@ class User(MenuEntry):
     
     def __str__(self) -> str:
         return self.username
+
+    def get_return_value(self) -> str:
+        return self.username
+
+
+class Any(MenuEntry):
+    def __str__(self) -> str:
+        return "Any"
+
+    def get_return_value(self) -> str:
+        return None
