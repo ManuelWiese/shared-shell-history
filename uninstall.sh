c7f7fd51059ffe46eda6448e2534e01885681c4e
Manuel Wiese <m.wiese@kaitos.ai>
2024-01-11 21:03:01 +0100

    Improved uninstall.sh a little

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,31 @@
+#!/bin/bash
+
+# Define the target directory
 TARGET_DIR="$HOME/.shared_shell_history"
-rm -fr $TARGET_DIR
-if [ $? ]; then
-    echo "Uninstall error: target folder not present"
+
+# Check if the target directory exists and remove it
+if [ -d "$TARGET_DIR" ]; then
+    rm -rf "$TARGET_DIR"
+    echo "Removed directory: $TARGET_DIR"
+else
+    echo "Uninstall error: Target folder not present."
+    exit 1
 fi
 
+# Define path to .bashrc
 BASHRC="$HOME/.bashrc"
 
+# Define the source string to be removed
 SOURCE_STRING="source $TARGET_DIR/shared_shell_history.sh"
 
-#Escapes slashes of the source string for usage in sed
-SOURCE_STRING=$(echo "$SOURCE_STRING" | sed 's/\//\\\//g')
+# Check if .bashrc contains the source string
+if grep -qF "$SOURCE_STRING" "$BASHRC"; then
+    # Use sed to delete the line containing the source string
+    # use | as delimiter
+    sed -i "\|$SOURCE_STRING|d" "$BASHRC"
+    echo "Removed source command from $BASHRC"
+else
+    echo "Source command not found in $BASHRC"
+fi
 
-sed -i "/^$SOURCE_STRING/D" $BASHRC
+echo "Uninstallation complete."
