457dfeccdbc7f307d4be0c1c411b5d61f586c0c0
Manuel Wiese <m.wiese@kaitos.ai>
2024-01-12 12:22:33 +0100

    Documentation of SelectionScreen

--- shared_shell_history/select_from_history/selection_screen.py
+++ shared_shell_history/select_from_history/selection_screen.py
@@ -6,18 +6,41 @@ from textual.widgets import Button, SelectionList
 
 
 class SelectionScreen(ModalScreen):
+    """
+    A custom screen for selecting items from a list.
+
+    This screen displays a list of items and allows the user to select one or more items.
+    It supports selecting all items, deselecting all, or manually selecting individual items.
+
+    Attributes:
+        title (str): The title of the selection screen.
+        values (list): A list of values to be displayed for selection.
+        currently_selected (list): A list of values that are currently selected.
+    """
     BINDINGS = [
         Binding("a", "select_all()", "Select All"),
         Binding("o", "select_one()", "Select One"),
     ]
 
     def __init__(self, title, values, currently_selected):
+        """
+        Initializes the SelectionScreen with a title, values for selection,
+        and currently selected items.
+
+        Args:
+            title (str): The title of the screen.
+            values (list): The values to be displayed for selection.
+            currently_selected (list): The values that are currently selected.
+        """
         super().__init__()
         self.title = title
         self.values = values
         self.currently_selected = currently_selected
 
     def compose(self):
+        """
+        Composes the SelectionList and Buttons for the screen.
+        """
         sorted_values = sorted(self.values)
         selection_list_items = (
             (value, value, value in self.currently_selected)
@@ -32,10 +55,19 @@ class SelectionScreen(ModalScreen):
             yield Button("OK", id="ok")
 
     def on_mount(self):
+        """
+        Called when the screen is mounted. Sets the border title of the selection list.
+        """
         self.query_one(SelectionList).border_title = self.title
 
     @on(Button.Pressed)
     def button_pressed(self, event):
+        """
+        Handles button press events.
+
+        Args:
+            event: The event object containing the button pressed.
+        """
         if event.button.id == "all":
             self.action_select_all()
         elif event.button.id == "one":
@@ -45,8 +77,14 @@ class SelectionScreen(ModalScreen):
             self.dismiss(selected)
 
     def action_select_all(self):
+        """
+        Selects all items in the selection list.
+        """
         self.query_one(SelectionList).select_all()
 
     def action_select_one(self):
+        """
+        Deselects all items and then selects the currently highlighted item in the selection list.
+        """
         self.query_one(SelectionList).deselect_all()
         self.query_one(SelectionList)._toggle_highlighted_selection()
